f0ed8c86d06a2cb8325dd759f85471d999f6fd08
ansonruan7 <ansonruan4@gmail.com>
2024-02-04 22:46:07 -0500

    setup basic structure for program status, fixed next.config

---
+++ student-center-fe/src/app/program-status/page.js
@@ -0,0 +1,24 @@
+'use client'
+
+import {React, useState} from 'react';
+import Header from '@/components/header';
+
+const program_status = () => {
+    return(
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-800 to-purple-500">
+            <div className="fixed top-0 left-0 w-full z-50">
+                <Header />
+            </div>
+            <div className='grid grid-cols-2 w-full'>
+                <div className='bg-white p-6 m-24 rounded-lg drop-shadow-lg'>
+                    <p className='text-black'>Hello</p>
+                </div>
+                <div className='bg-white p-6 m-24 rounded-lg drop-shadow-lg'>
+                    <p className='text-black'>Hello</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default program_status;
