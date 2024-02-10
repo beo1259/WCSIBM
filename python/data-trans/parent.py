d8a165c6cd65f1936eac6d3d651b024e093ac99d
beo1259 <oneilb123@gmail.com>
2024-02-10 12:27:21 -0600

    parent script running indefinitely

--- python/data-trans/parent.py
+++ python/data-trans/parent.py
@@ -5,5 +5,5 @@ import subprocess
 
 while True:
     subprocess.call(["/home/linux1/WCSIBM/python/data-trans/student-trans.py"])
-    subprocess.call(["/home/linux1/WCSIBM/python/data-trans/professor-trans.py"])
+    subprocess.call(["/home/linux1/WCSIBM/python/data-trans/proffesor-trans.py"])
     time.sleep(172800)
