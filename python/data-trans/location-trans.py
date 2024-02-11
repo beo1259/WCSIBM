3d9705e60c42bea06485849afcbc1c63c34e2f4b
Ayan <ayansingal676@gmail.com>
2024-02-10 23:08:47 -0500

    Added table transfer scripts
    Specifically for:
    Labs
    Lectures
    Locations
    Courses
    
    NOTE: Please test if they work right on your machine, thanks.

---
+++ python/data-trans/location-trans.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import ibm_db
+import ftplib
+
+# Function to convert a record to a fixed-width format
+def format_fixed_width(record):
+    formatted_record = ''                                  # RECTIFY CHAR COUNT IN THE COMMENTS 
+    formatted_record += str(record['LOCATIONID']).ljust(10)  # LocationID, left-justified, 15 characters
+    formatted_record += str(record['LOCATIONNAME']).ljust(60)            # LocationName, 19 characters
+
+    return formatted_record + '\n'  # Add a newline at the end of each record
+
+# Connection string for Db2
+conn_string = (
+    "DATABASE=WCS2024;"
+    "HOSTNAME=localhost;"
+    "PORT=50000;"
+    "PROTOCOL=TCPIP;"
+    "UID=db2inst1;"
+    "PWD=Zmframewcs_54379@;"
+)
+
+# Connect to the database
+conn = ibm_db.connect(conn_string, '', '')
+
+# SQL Query
+select = "SELECT * FROM STUCENTR.LOCATION"
+
+# Execute the query
+stmt = ibm_db.exec_immediate(conn, select)
+
+# Local file for saving query results
+local_file = '/home/linux1/WCSIBM/python/data-trans/output/location_data.txt'
+
+# Open the file for writing
+with open(local_file, 'w') as file:
+    row = ibm_db.fetch_assoc(stmt)
+    while row:
+        # Convert each row to fixed-width format and write to the file
+        file.write(format_fixed_width(row))
+        row = ibm_db.fetch_assoc(stmt)
+
+# Close the Db2 connection
+ibm_db.close(conn)
+
+# FTP details
+ftp_hostname = '204.90.115.200'
+ftp_username = 'z41780'
+ftp_password = 'GEM31DUN'
+remote_file_path = '/z/z41780/WCS-IBM/data/location_data.txt'
+
+# FTP transfer
+with ftplib.FTP(ftp_hostname) as ftp:
+    ftp.login(ftp_username, ftp_password)
+    with open(local_file, 'rb') as file:
+        ftp.storbinary(f'STOR {remote_file_path}', file)
+
+print(file)
+
+print("File transferred successfully.")
+
