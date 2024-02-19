a459f55a3680048a3f7a1b0118989b64069e09cb
Chidubem <canekwu2@uwo.ca>
2024-02-19 14:15:49 -0500

    Updated to sort courses by the year user chooses

--- student-center-fe/src/components/viewGrades.js
+++ student-center-fe/src/components/viewGrades.js
@@ -3,10 +3,15 @@ import axios from 'axios';
 
 const GradesPage = () => {
   const [grades, setGrades] = useState([]);
+  const [chosenYear, setChosenYear] = useState('');
+
+  const handleYearChange = (event) => {
+    setChosenYear(event.target.value);
+  };
 
   useEffect(() => {
     
-    const fetchGrades = async () => {
+const fetchGrades = async () => {
         let stuCalendarID = sessionStorage.getItem('studentId');
        
         try {
@@ -34,27 +39,42 @@ const GradesPage = () => {
       fetchGrades();
   }, []);
 
+// Getting the min and max year of the student to create the list for grades they can visit
+const minYear = Math.min(...grades.map(grade => parseInt(grade.YEAR)));
+const maxYear = Math.max(...grades.map(grade => parseInt(grade.YEAR)));
+const yearOptions = [];
+
+for (let year = minYear; year <= maxYear; year++) {
+  yearOptions.push(<option key={year} value={year}>{year}</option>);
+}
+
   return (
     <div>
-      <h1>Student Grades</h1>
+      <h1>View Grades</h1>
+        <div className="mt-6">
+                <label className='font-bold' id="cata">Select Term: </label>
+                <select id="yearReqs"  onChange={handleYearChange}>
+                    {yearOptions}
+                </select>
+        </div>
       <table>
         <thead>
           <tr>
             <th>Subject</th>
             <th>Grade</th>
-            <th>Semester</th>
+            {/* <th>Semester</th> */}
           </tr>
         </thead>
         <tbody>
         {/* mapping function to iterate through each course and perform get name, grade and semester.  */}
           {grades.map((grade, index) => {
             {/* Found out you gotta use curly brackets with return statement if you use an if statement within. Used to filter by year*/}
-            if(grade.GRADE>80){
+            if(grade.YEAR==chosenYear){
             return(
             <tr key={index}>
               <td>{grade.COURSENAME}</td>
               <td>{grade.GRADE}</td>
-              <td>{grade.GRADE}</td>
+              {/* <td>{grade.SEMESTER}</td> */}
             </tr>
           )
             }}
