296e8741a71e21295bde5d64a265e0cad3c247a0
Brayden Edward O'Neil <boneil45@uwo.ca>
2024-03-03 12:57:55 -0500

    added python script for prereq check

---
+++ student-center-fe/exec-prereq.py
@@ -0,0 +1,29 @@
+import paramiko
+import sys
+
+hostname = '204.90.115.200'
+port = 22
+username = 'z41780'
+password = 'TEA03POX'
+java_class_directory = '/z/z41780/WCS-IBM/java'
+java_class_name = 'Prereq' 
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+def main(studentId):
+    try:
+        client.connect(hostname, port=port, username=username, password=password)
+
+        stdin, stdout, stderr = client.exec_command(f'cd {java_class_directory} && /usr/lpp/java/J8.0_64/bin/java -cp . {java_class_name} {studentId}')
+        
+        print(stdout.read().decode().strip())
+        sys.stdout.flush()
+            
+    finally:
+        client.close()
+        
+if __name__ == "__main__":
+    main(823321975)
+else: 
+    sys.exit(1)
